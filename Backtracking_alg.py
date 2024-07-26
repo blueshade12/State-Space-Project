fd5d790f9886e5bc2ae344f2666685a48ebcee37
blueshade12 <150080779+blueshade12@users.noreply.github.com>
2024-07-26 10:03:36 -0400

    backtracking_alg.py

--- Backtracking_alg.py
+++ Backtracking_alg.py
@@ -24,15 +24,16 @@ class BacktrackingAlgorithm(Generic[VariableT]):
         unassigned = [v for v in self.csp.variables if v not in assignment]
         return min(unassigned, key=lambda var: len(self.csp.domains[var]))
 
+    def setup_sudoku(puzzle: str) -> Tuple[List[Tuple[int, int]], Dict[Tuple[int, int], Set[int]], Dict[Tuple[int, int], int]]:
+      variables = [(r, c) for r in range(9) for c in range(9)]
+      domains = {var: set(range(1, 10)) for var in variables}
+      initial_assignment = {}
 
-    def solve_sudoku(self, puzzle):
-      self.variables = [(i, j) for i in range(9) for j in range(9)]
-      domains = {var: set(range(1, 10)) for var in self.variables}
-      for i in range(9):
-          for j in range(9):
-            if puzzle[i][j] != 0:
-                domains[(i, j)] = {puzzle[i][j]}
-      csp = CSP(self.variables, domains)
-      csp.add_constraint(SudokuConstraint(self.variables))
-      solver = BacktrackingAlgorithm(csp)
-      return solver.backtracking_search()
+      for r in range(9):
+          for c in range(9):
+              value = int(puzzle[r * 9 + c])
+              if value != 0:
+                  initial_assignment[(r, c)] = value
+                  domains[(r, c)] = {value}
+
+      return variables, domains, initial_assignment
