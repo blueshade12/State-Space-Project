e20459871289803a0ba99b4e50c07f5c83cd9263
Divita211 <97330963+Divita211@users.noreply.github.com>
2024-07-26 10:08:50 +0800

    Update sudoku_reader.py

--- sudoku_reader.py
+++ sudoku_reader.py
@@ -1,8 +1,15 @@
-class Sudoku_reader():
-    def sudoku_reader(file_path):
+class Sudoku_reader:
+    def sudoku_reader(self, file_path):
         puzzle = []
-        with open(file_path, 'r') as file:
-            for line in file:
-                row = list(map(int, line.strip().split()))
+        with open(file_path, 'r') as f:
+            for line in f:
+                row = []
+                for value in line:
+                    if value.isspace() or value == '\n':
+                        row.append(0)
+                    elif value.isdigit():
+                        row.append(int(value))
+                    else:
+                        raise ValueError(f"Invalid character in sudoku file. Replace unknown with 0")
                 puzzle.append(row)
         return puzzle
