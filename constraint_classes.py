e4e3ba4f4262edc0de5b6332a6475586c0e11135
Divita211 <97330963+Divita211@users.noreply.github.com>
2024-07-27 10:58:57 +0800

    Update constraint_classes.py

--- constraint_classes.py
+++ constraint_classes.py
@@ -1,3 +1,9 @@
+from typing import Generic, TypeVar, List, Dict, Tuple, Optional
+from abc import ABC, abstractmethod
+
+VariableT = TypeVar("VariableT")
+AssignmentT = Dict[VariableT, int]
+
 class Constraint(Generic[VariableT], ABC):
     def __init__(self, scope: List[VariableT]) -> None:
         self.scope = scope
@@ -6,8 +12,11 @@ class Constraint(Generic[VariableT], ABC):
     def satisfied(self, assignment: AssignmentT) -> bool:
         pass
 
-
 class SudokuConstraint(Constraint[Tuple[int, int]]):
+    def __init__(self) -> None:
+        scope = [(row, col) for row in range(9) for col in range(9)]
+        super().__init__(scope)
+
     def satisfied(self, assignment: AssignmentT) -> bool:
         def unique(values: List[int]) -> bool:
             values = [value for value in values if value != 0]
@@ -19,7 +28,7 @@ class SudokuConstraint(Constraint[Tuple[int, int]]):
         for col in range(9):
             if not unique([assignment.get((row, col), 0) for row in range(9)]):
                 return False
-        
+
         for grid_row in range(3):
             for grid_col in range(3):
                 if not unique([assignment.get((row, col), 0)
@@ -27,3 +36,27 @@ class SudokuConstraint(Constraint[Tuple[int, int]]):
                                for col in range(grid_col * 3, (grid_col + 1) * 3)]):
                     return False
         return True
+
+class CSP(Generic[VariableT]):
+    def __init__(self, variables: List[VariableT], domains: Dict[VariableT, List[int]]) -> None:
+        self.variables = variables
+        self.domains = domains
+        self.constraints: Dict[VariableT, List[Constraint[VariableT]]] = {}
+        for variable in self.variables:
+            self.constraints[variable] = []
+
+    def add_constraint(self, constraint: Constraint[VariableT]) -> None:
+        for variable in constraint.scope:
+            if variable in self.variables:
+                self.constraints[variable].append(constraint)
+            else:
+                raise ValueError("Variable in constraint not in CSP")
+
+    def consistent(self, variable: VariableT, assignment: AssignmentT) -> bool:
+        for constraint in self.constraints[variable]:
+            if not constraint.satisfied(assignment):
+                return False
+        return True
+
+    def complete(self, assignment: AssignmentT) -> bool:
+        return len(assignment) == len(self.variables)
